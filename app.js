7d8cdf99c90006b22daade280419d5fa05b2eb29
Thet Oo Aung <thetooaung07@gmail.com>
2020-12-28 20:31:08 +0630

    Array IS

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ var number1 = 5;
 var number2 = 3.8;
 var result = add(number1, number2);
 console.log(result);
+var person = {
+    //   name: string,
+    //   age: number
+    // } = {
+    name: "Thet Oo Aung",
+    age: 19,
+    hobbies: ['Coding', 'Sports']
+};
+console.log(person.age);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -7,3 +7,21 @@ const number2 = 3.8;
 const result = add(number1, number2);
 
 console.log(result);
+
+
+const person = {
+//   name: string,
+//   age: number
+// } = {
+  name: "Thet Oo Aung",
+  age: 19,
+  hobbies: ['Coding', 'Sports']
+
+}
+
+console.log(person.age);
+
+for(const hobby of person.hobbies){
+  console.log(hobby.toUpperCase());
+  
+}
