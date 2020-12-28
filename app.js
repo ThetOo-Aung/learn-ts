fff7b266b539e5bb526f81923fc45a8f525bc792
Thet Oo Aung <thetooaung07@gmail.com>
2020-12-28 21:06:01 +0630

    Enum Ts

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-function add(num1, num2) {
-    return num1 + num2;
+/* function add( num1: number , num2 : number) {
+  return num1 + num2;
 }
-var number1 = 5;
-var number2 = 3.8;
-var result = add(number1, number2);
+const number1 = 5;
+const number2 = 3.8;
+
+const result = add(number1, number2);
+
 console.log(result);
+
+
+const person :{
+  name: string,
+  age: number,
+  hobbies: string[],
+  role: [number, string]
+} = {
+  name: "Thet Oo Aung",
+  age: 19,
+  hobbies: ['Coding', 'Sports'],
+  role: [2, 'author']
+
+}
+
+let favHobbies : any[] = ['hello', 'world', 20]
+
+console.log(person.age);
+
+for(const hobby of person.hobbies){
+  console.log(hobby.toUpperCase());
+  
+}
+
+
+ */
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
-    //   name: string,
-    //   age: number
-    // } = {
     name: "Thet Oo Aung",
     age: 19,
-    hobbies: ['Coding', 'Sports']
+    hobbies: ['Coding', 'Sports'],
+    role: Role.ADMIN
 };
-console.log(person.age);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+if (person.role === Role.ADMIN) {
+    console.log('is authorized');
 }
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-function add( num1: number , num2 : number) {
+/* function add( num1: number , num2 : number) {
   return num1 + num2;
 }
 const number1 = 5;
@@ -9,19 +9,41 @@ const result = add(number1, number2);
 console.log(result);
 
 
-const person = {
-//   name: string,
-//   age: number
-// } = {
+const person :{
+  name: string,
+  age: number,
+  hobbies: string[],
+  role: [number, string]
+} = {
   name: "Thet Oo Aung",
   age: 19,
-  hobbies: ['Coding', 'Sports']
+  hobbies: ['Coding', 'Sports'],
+  role: [2, 'author']
 
 }
+
+let favHobbies : any[] = ['hello', 'world', 20]
 
 console.log(person.age);
 
 for(const hobby of person.hobbies){
   console.log(hobby.toUpperCase());
+  
+}
+
+
+ */
+enum Role {ADMIN , READ_ONLY, AUTHOR};
+
+const person = {
+  name: "Thet Oo Aung",
+  age: 19,
+  hobbies: ['Coding', 'Sports'],
+  role: Role.ADMIN,
+
+}
+
+if(person.role === Role.ADMIN){
+  console.log('is authorized');
   
 }
