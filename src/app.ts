53e7c0bebca0876ade7abf96da0af5e06f1a6a6a
Thet Oo Aung <thetooaung07@gmail.com>
2020-12-29 19:45:18 +0630

    details about tsconfig.json

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,1 @@
-/* unknown is just like any but you can't assign unknown variable to other variable like let userName = userInput without if checking */
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = "Max";
-// userName = userInput;
-
-
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-
-function generateError(message: string , code: number ): never{
-  throw{message: message, errorCode: code}
-}
-generateError("An Error Occoured", 500);
+console.log("Hello");
