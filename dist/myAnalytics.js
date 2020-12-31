2dff1522edfa1fa024dd718a08180a6a4b53f668
Thet Oo Aung <thetooaung07@gmail.com>
2020-12-31 12:16:28 +0630

    Add Random words on btn Click

--- dist/myAnalytics.js
+++ dist/myAnalytics.js
@@ -1,2 +1,12 @@
 "use strict";
 console.log("Analyzing ...");
+var strArr = [
+    'a', 'o', 'e', 'i', 'u', 'u*',
+    'b', 'p', 'm', 'f', 'd', 't', 'n', 'l', 'g', 'k', 'h', 'j', 'q', 'x', 'zh', 'ch', 'sh', 'r', 'z', 'c', 's', 'y', 'w',
+    'ai', 'ei', 'ui', 'ao', 'ou', 'iu',
+    'ie', 'u*e', 'er', 'an', 'ang', 'en', 'eng', 'in', 'ing', 'ong', 'un u*n'
+];
+var button = document.querySelector('button');
+button.addEventListener('click', function () {
+    button.innerText = strArr[Math.floor(Math.random() * strArr.length)];
+});
--- src/myAnalytics.ts
+++ src/myAnalytics.ts
@@ -1,1 +1,19 @@
 console.log("Analyzing ...")
+
+const strArr : string[] = [
+
+  'a', 'o', 'e','i', 'u', 'u*', 
+
+  'b','p','m','f','d','t','n','l','g','k','h','j','q','x','zh','ch','sh','r','z','c','s','y','w',
+  
+  'ai','ei','ui','ao','ou','iu',
+
+  'ie','u*e','er', 'an','ang','en','eng','in','ing','ong','un',' u*n'
+]
+
+const button = document.querySelector('button') ! ;
+
+button.addEventListener('click', () => {
+  button.innerText = strArr[Math.floor(Math.random() * strArr.length)];
+})
+  
